91a65492a682beda43c830104b5b0ebc911b71d0
Benjamin Morali <benjaminmorali4@hotmail.fr>
2017-02-05 17:17:30 +0100

    Add FenceLever script

---
+++ Assets/FenceLever.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FenceLever : MonoBehaviour {
+    //private GameController gameController;
+
+    void Start()
+    {
+        /*GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null)
+            gameController = gameControllerObject.GetComponent<GameController>();
+        if (gameController == null)
+            Debug.Log("Cannot find 'GameController' script");*/
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        print("Enter !");
+        if (other.CompareTag("Lever"))
+        {
+            //Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            //gameController.GameOver();
+            print("LOLOLOLOL");
+        }
+        //gameController.AddScore(scoreValue);
+        //Destroy(other.gameObject);
+        //Destroy(gameObject);
+    }
+}
