4506bafbb6a77a51ac8f7ad52f663dfd5171de32
Benjamin Morali <benjaminmorali4@hotmail.fr>
2017-02-05 18:52:22 +0100

    Replace cube with key

---
+++ Assets/Scripts/FenceKey.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FenceLever : MonoBehaviour {
+    void OnTriggerEnter(Collider other)
+    {
+        print("Enter !");
+        Destroy(gameObject);
+        Destroy(GameObject.Find("Island/Lighthouse/Fence"));
+    }
+}
