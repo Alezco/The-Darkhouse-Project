72716c7bfb4fa686a402285d1c17e96cf25f0245
Benjamin Morali <benjaminmorali4@hotmail.fr>
2017-02-05 17:33:31 +0100

    Add collision action with fence

--- Assets/FenceLever.cs
+++ Assets/FenceLever.cs
@@ -3,28 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FenceLever : MonoBehaviour {
-    //private GameController gameController;
-
-    void Start()
-    {
-        /*GameObject gameControllerObject = GameObject.FindWithTag("GameController");
-        if (gameControllerObject != null)
-            gameController = gameControllerObject.GetComponent<GameController>();
-        if (gameController == null)
-            Debug.Log("Cannot find 'GameController' script");*/
-    }
-
     void OnTriggerEnter(Collider other)
     {
         print("Enter !");
-        if (other.CompareTag("Lever"))
-        {
-            //Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
-            //gameController.GameOver();
-            print("LOLOLOLOL");
-        }
-        //gameController.AddScore(scoreValue);
-        //Destroy(other.gameObject);
-        //Destroy(gameObject);
+        Destroy(gameObject);
+        Destroy(GameObject.Find("Island/Lighthouse/Fence"));
     }
 }
