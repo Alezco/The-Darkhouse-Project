ac5182ef30c0ca424a468f8e1413425da952b12b
Guillaume Normand <guillaume.normand@epita.fr>
2017-02-07 23:56:18 +0100

    add enemy boat to first scene

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed;
+
+    private Transform playerTransform;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerTransform = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        catchPlayer();
+    }
+
+    void catchPlayer()
+    {
+        transform.LookAt(playerTransform);
+        if (Vector3.Distance(transform.position, playerTransform.position) >= 0)
+        {
+            transform.position += transform.forward * speed;
+        }
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Scenes/BoatScene");
+        }
+        
+    }
+}
