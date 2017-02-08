4d61e58f1ee212879a9de2554bfea1ec81adedf9
Guillaume Normand <guillaume.normand@epita.fr>
2017-02-08 14:11:03 +0100

    add functionnality to look at other boat

--- Assets/Scripts/BoatController.cs
+++ Assets/Scripts/BoatController.cs
@@ -9,11 +9,20 @@ public class BoatController : MonoBehaviour {
 
     void FixedUpdate()
     {
+        lookAtEnemy();
         float horizontal = Input.GetAxis("Horizontal");
         transform.Rotate(0, horizontal * speedX, 0);
         float vertical = Input.GetAxis("Vertical");
         transform.Translate(0, 0, vertical * speedY);
         float mouseX = Input.GetAxis("Mouse X");
         transform.Rotate(0, mouseX * sensitivityX, 0);
+    }
+
+    void lookAtEnemy()
+    {
+        if (Input.GetKey(KeyCode.Space))
+            Camera.main.gameObject.transform.LookAt(GameObject.FindWithTag("Enemy").transform);
+        else
+            Camera.main.gameObject.transform.LookAt(transform);
     }
 }
