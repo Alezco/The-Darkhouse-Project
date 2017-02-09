c6eb75ca718077fc33205a8518416fcfb6f93ec0
Guillaume Normand <guillaume.normand@epita.fr>
2017-02-09 22:36:44 +0100

    add goal hud in lighthouse scene

---
+++ Assets/UpdateHUDGoal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UpdateHUDGoal : MonoBehaviour {
+
+    public Text goalText;
+    public string newGoal;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            goalText.text = newGoal;
+        }
+    }
+}
