0cf117b7e1fabafea6c927f2aafb082cbfd73b5f
Benjamin Morali <benjaminmorali4@hotmail.fr>
2017-02-10 02:10:05 +0100

    Update room 2

---
+++ Assets/Scripts/LighterController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LighterController : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        print("test");
+        PlayerInventory.getInstance().getInventory().Add("Lighter");
+        Destroy(gameObject);
+    }
+
+	void Update () {
+        transform.Rotate(0, 2, 0);
+	}
+}
---
+++ Assets/Scripts/TorchController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchController : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other)
+    {
+        Texture3D texture = (Texture3D) Resources.Load("Textures/fire");
+        gameObject.GetComponent<Renderer>().material.mainTexture = texture;
+    }
+}
