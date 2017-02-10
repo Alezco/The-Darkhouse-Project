6714eac8766251eee0338ce140939194d68385fd
Benjamin Morali <benjaminmorali4@hotmail.fr>
2017-02-10 12:08:30 +0100

    Add final room

---
+++ Assets/Scripts/FinalWallController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalWallController : MonoBehaviour {
+
+	void OnTriggerExit(Collider other)
+    {
+        gameObject.GetComponent<MeshRenderer>().enabled = true;
+        gameObject.GetComponent<BoxCollider>().isTrigger = false;
+    }
+}
