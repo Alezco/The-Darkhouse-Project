f036dcb4ca3b0d444868962b857b590a31e72b41
Guillaume Normand <guillaume.normand@epita.fr>
2017-02-10 12:48:44 +0100

    add final event

---
+++ Assets/ChestController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestController : MonoBehaviour {
+
+    public Camera finalCamera;
+
+	// Use this for initialization
+	void Start () {
+        finalCamera.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            print("opzae");
+            finalCamera.gameObject.SetActive(true);
+            collider.gameObject.SetActive(false);
+        }
+    }
+}
